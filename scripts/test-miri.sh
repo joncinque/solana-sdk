21e0f39b92f2a1fcd8b71e1b52e26a32b0ec610c
febo <febo@anza.xyz>
2025-11-04 15:05:04 +0000

    Fixed solana-hash crate version

--- scripts/test-miri.sh
+++ scripts/test-miri.sh
@@ -5,4 +5,8 @@ here="$(dirname "$0")"
 src_root="$(readlink -f "${here}/..")"
 cd "${src_root}"
 # miri is very slow; so only run very few of selective tests!
-./cargo nightly miri test -p solana-hash -p solana-account-info
+#
+# Note: fixed version of solana-hash is needed here to avoid
+#       dependency resolution issues since v3 just re-exports
+#       v4 of the `solana-hash` crate.
+./cargo nightly miri test -p solana-hash@3.0.0 -p solana-account-info
